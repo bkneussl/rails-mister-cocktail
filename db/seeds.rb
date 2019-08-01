b2c332d71d288e276d8edc58e3de58709e2738bf
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-01 17:18:32 +0800

    Mister Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = open(url).read
+ingredient = JSON.parse(ingredients)
+
+ingredient["drinks"].each do |ingredients|
+  Ingredient.create(name: ingredients["strIngredient1"])
+end
+
